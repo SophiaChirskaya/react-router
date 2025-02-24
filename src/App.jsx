fb579883f8353ff01c3616760e38ef2b83a60aa7
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-24 16:01:23 +0100

    import 3 new pages to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 
 import './App.css'
 import Blog from './components/Blog'
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import ChiSiamo from "./pages/ChiSiamo";
+import PostsList from "./pages/PostsList";
 
 
 function App() {
