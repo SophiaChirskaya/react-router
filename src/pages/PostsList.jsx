b227dd921289da5acba9ffa280ee33c32cfad9e9
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-24 16:00:10 +0100

    [feat] 3 new pages

---
+++ src/pages/PostsList.jsx
@@ -0,0 +1,10 @@
+import BlogForm from "./../components/BlogForm";
+
+export default function PostsList() {
+    return (
+        <>
+        <h2>Lista dei posts</h2>
+        <PizzaForm />
+        </>
+    );
+}
