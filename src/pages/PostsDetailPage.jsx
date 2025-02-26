c1d2712e0ec3f0605ed51ad4cff3a6ec023479a9
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-26 05:29:10 +0100

    [feat]PostsDetailPage

---
+++ src/pages/PostsDetailPage.jsx
@@ -0,0 +1,10 @@
+import BlogsList from "./../components/BlogsList";
+
+export default function PostsDetailPage() {
+    return (
+        <>
+        <h2>Blog di cucina</h2>
+        <BlogsList />
+        </>
+    )
+}
